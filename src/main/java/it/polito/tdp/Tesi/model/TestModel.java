ac54d01d4e8dcb1e0922112431e81c9496916ca8
Sylvie Molinatto <100536489+sylviemolinatto@users.noreply.github.com>
2022-10-28 09:40:17 +0200

    Delete TestModel.java

--- src/main/java/it/polito/tdp/Tesi/model/TestModel.java
+++
@@ -1,9 +0,0 @@
-package it.polito.tdp.Tesi.model;
-
-public class TestModel {
-
-	public static void main(String[] args) {
-	Model model = new Model();
-	System.out.println(model.getJobTitles());
-	}
-}
